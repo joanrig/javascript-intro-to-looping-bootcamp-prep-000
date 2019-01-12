09052998432ea902f90a55e40d8163aa0b8d5f7c
Joan in DC <joanrigdon@gmail.com>
2019-01-12 22:26:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,6 +18,14 @@ function whileLoop(number) {
 
 
 function doWhileLoop(array) {
+  do {
+    array.pop;
+    incrementVariable();
+  
+  var i = 0
+  function incrementVariable() {
+    i = i + 1;
+  }
   
 }
 
