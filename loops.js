a5ca31881c7a40fd7794c2169635b21a5719b131
Joan in DC <joanrigdon@gmail.com>
2019-01-12 22:09:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,31 +1,41 @@
-function forLoop(array) {
-  for (let i = 0; i < 25; i++){
-    if (i === 1) {
-      array.push(`I am 1 strange loop.`);
-    } else {
-      array.push(`I am ${i} strange loops.`);
-      }
-    }
-    return array;
+function forLoop(array)
+  for (let i=0; i<26; i++) {
+    if (i===1) {
+      array.push('I am 1 strange loop.')
+    else 
+      array.push(`I am ${i} strange loops.`)
   }
+
+
+
+// function forLoop(array) {
+//   for (let i = 0; i < 25; i++){
+//     if (i === 1) {
+//       array.push(`I am 1 strange loop.`);
+//     } else {
+//       array.push(`I am ${i} strange loops.`);
+//       }
+//     }
+//     return array;
+//   }
   
 
-let number = 75;
-function whileLoop(number) {
-while (number > 0) {
-  console.log(--number);
-    }
-  return "done";
-}
+// let number = 75;
+// function whileLoop(number) {
+// while (number > 0) {
+//   console.log(--number);
+//     }
+//   return "done";
+// }
 
 
-function doWhileLoop(array) {
-   function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-   do {
-    array.pop();
-  } while (array.length > 0 && maybeTrue());
-   return array;
-}
+// function doWhileLoop(array) {
+//   function maybeTrue() {
+//     return Math.random() >= 0.5
+//   }
+//   do {
+//     array.pop();
+//   } while (array.length > 0 && maybeTrue());
+//   return array;
+// }
 
